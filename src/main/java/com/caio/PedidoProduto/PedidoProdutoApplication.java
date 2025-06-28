f02314381b654b914cd4fae31b80b2c7dafcdaec
kaiohp-exe <Kaio.pradoborin@gmail.com>
2025-06-28 11:56:11 -0300

    comeco

---
+++ src/main/java/com/caio/PedidoProduto/PedidoProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.caio.PedidoProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoProdutoApplication.class, args);
+	}
+
+}
